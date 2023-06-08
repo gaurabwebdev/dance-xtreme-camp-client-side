2054adcf5742d5563fdca267bd9057f0bc49fb5e
Simi Sur <gaurabbose40@gmail.com>
2023-06-08 20:08:21 +0600

    useadmin hook modified

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -7,7 +7,7 @@ import useAxios from "./useAxios";
 const useAdmin = () => {
   const { user } = useAuth();
   const [axiosSecure] = useAxios();
-  const { data: isAdmin = [], isLoading: isAdminLoading } = useQuery({
+  const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
     queryKey: ["isAdmin", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/admin/${user?.email}`);
