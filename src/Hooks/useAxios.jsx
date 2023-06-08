8554e3ee6c6345485a685bbc4dd5f34afb411b70
gaurabwebdev <gaurabbose40@gmail.com>
2023-06-08 16:29:46 +0600

    axios interceptors created

---
+++ src/Hooks/useAxios.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const useAxios = () => {
+  const { userLogOut } = useAuth();
+  const navigate = useNavigate();
+  const axiosSecure = axios.create({
+    baseURL: "http://localhost:5000",
+  });
+
+  useEffect(() => {
+    axiosSecure.interceptors.request.use((config) => {
+      const jToken = localStorage.getItem("accessJwt");
+      if (jToken) {
+        config.headers.Authorization = `Bearer ${jToken}`;
+      }
+      return config;
+    });
+
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (
+          (error.response && error.response === 401) ||
+          error.response === 403
+        ) {
+          await userLogOut();
+          navigate("/login");
+        }
+      }
+    );
+  }, [userLogOut, navigate, axiosSecure]);
+
+  return [axiosSecure];
+};
+
+export default useAxios;
