6084b2ae0ffbf9323e7b12c916ea30ae8d9490ea
Simi Sur <gaurabbose40@gmail.com>
2023-06-10 15:26:14 +0600

    class page cover displayed

---
+++ src/Components/Shared/PageCover/PageCover.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PageCover = ({ coverDetails }) => {
+  const { title, sub_title, cover_url } = coverDetails;
+  console.log(coverDetails.cover_url);
+  return (
+    <div
+      className="bg-cover bg-top bg-no-repeat h-[350px]"
+      style={{ backgroundImage: `url(${cover_url})` }}
+    >
+      <div className="w-full h-full backdrop-brightness-50 flex flex-col justify-center items-center">
+        <div className="uppercase p-5 bg-secondary bg-opacity-30 rounded-lg text-center">
+          <h2 className="text-6xl  font-semibold">{title}</h2>
+          <h4 className="text-3xl font-normal my-3 lett tracking-wider">
+            Home | {sub_title}
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageCover;
--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import approvedClasses from "../../Hooks/approvedClasses";
+import PageCover from "../../Components/Shared/PageCover/PageCover";
 
 const Classes = () => {
   const [allApprovedClasses, refetch] = approvedClasses();
   console.log(allApprovedClasses);
+  const coverDetails = {
+    title: "our classes",
+    sub_title: "classes",
+    cover_url: "https://i.ibb.co/CVSwc2y/class-page-cover-1.jpg",
+  };
   return (
     <div>
+      <PageCover coverDetails={coverDetails} />
       <p>All Classes</p>
     </div>
   );
