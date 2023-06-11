5fd0c0495323f8cb2e65d502419dcb315fbd66c8
Simi Sur <gaurabbose40@gmail.com>
2023-06-11 14:43:52 +0600

    stripe integrated & activated with client side

---
+++ src/Components/Shared/CheckOutForm/CheckOutForm.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { FaCheckCircle, FaHandHoldingUsd } from "react-icons/fa";
+import useAxios from "../../../Hooks/useAxios";
+import useAuth from "../../../Hooks/useAuth";
+
+const CheckOutForm = ({ totalPrice }) => {
+  const { user } = useAuth();
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [paymentProcessing, setPaymentProcessing] = useState(false);
+  const [trnxId, setTrnxId] = useState("");
+  const [axiosSecure] = useAxios();
+  useEffect(() => {
+    if (totalPrice > 0) {
+      axiosSecure
+        .post(`/create-payment-intent`, { totalPrice })
+        .then((data) => {
+          if (data.data.clientSecret) {
+            setClientSecret(data.data.clientSecret);
+          }
+        });
+    }
+  }, [totalPrice]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("connected");
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    console.log(card);
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      console.log("Error ", error);
+      setCardError(error.message);
+    } else {
+      console.log("Payment Mehtod ", paymentMethod);
+      setCardError("");
+    }
+    setPaymentProcessing(true);
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "N/A",
+            name: user?.displayName || "Anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log(confirmError);
+    }
+    console.log(paymentIntent);
+    setPaymentProcessing(false);
+    if (paymentIntent.status === "succeeded") {
+      const transactionId = paymentIntent.id;
+      setTrnxId(transactionId);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <div className=" text-center mt-16">
+          <button
+            className="btn btn-secondary btn-sm"
+            type="submit"
+            disabled={!stripe || !clientSecret || paymentProcessing}
+          >
+            <FaHandHoldingUsd className="text-xl" />
+            Confirm Payment
+          </button>
+        </div>
+      </form>
+      {cardError && <p className="text-xl text-red-500 mt-8">{cardError}</p>}
+      {trnxId && (
+        <p className="text-xl text-success mt-8">
+          Payment Completed. Your transaction id is: {trnxId}
+        </p>
+      )}
+    </>
+  );
+};
+
+export default CheckOutForm;
--- src/Pages/DashboardContent/MakePayment/MakePayment.jsx
+++ src/Pages/DashboardContent/MakePayment/MakePayment.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import { CardElement, Elements, useElements, useStripe } from "../../src";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "../../../Components/Shared/CheckOutForm/CheckOutForm";
+import useCart from "../../../Hooks/useCart";
+
+const stripePromise = loadStripe(`${import.meta.env.VITE_STRIPE_PK}`);
 
 const MakePayment = () => {
+  const [, myCart] = useCart();
+  const totalPrice = parseFloat(
+    myCart.reduce((sum, cartItem) => sum + cartItem.price, 0).toFixed(2)
+  );
+  console.log(totalPrice);
   return (
     <div>
       <p>Make Payment</p>
+
+      <div>
+        <div className="max-w-3xl mx-auto my-20 border-2 border-secondary p-16 bg-gray-900 rounded-lg">
+          <Elements stripe={stripePromise}>
+            <CheckOutForm totalPrice={totalPrice} />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
 };
